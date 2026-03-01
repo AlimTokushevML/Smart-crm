42d8641a56d8007e3c396adb0da0e48e07c56c6e
AlimJan <limon.tokushev@gmail.com>
2026-03-01 19:06:14 +0500

    Initial setup: Express server with test route

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.json({ message: 'smart-crm is running' });
+});
+app.listen(PORT, () => {
+    console.log(`server is running on ${PORT}`);
+});
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+res.json ({message: 'smart-crm is running'});
+});
+
+app.listen(PORT, () => {
+console.log(`server is running on ${PORT}`)
+});
