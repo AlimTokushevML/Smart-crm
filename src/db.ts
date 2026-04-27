53bf7e94d4e430e2f307174c428cef2980a33e71
AlimJan <limon.tokushev@gmail.com>
2026-04-27 19:30:11 +0500

    Add SSL support for Railway PostgreSQL

--- src/db.ts
+++ src/db.ts
@@ -10,6 +10,7 @@ export const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: Number(process.env.DB_PORT),
+    ssl: process.env.DB_HOST === 'localhost' ? false : { rejectUnauthorized: false }
 });
 
 
